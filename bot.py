b3e73a84538a267ba0b278ac47bb894071fbb8d9
Zach Seifts <zach.seifts@gmail.com>
2010-03-09 15:39:58 -0500

    removing logging. will fix later.

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@
 '''
 
 import os
-import logging
 
 from lib.objects import Weather, BooneWeather
 import private
@@ -12,14 +11,8 @@ import private
 
 username = private.USERNAME
 password = private.PASSWORD
-pickle_file = private.PICKLEFILE
 
 if __name__ == '__main__':
     format = '%(asctime)s %(name)-25s %(levelname)-8s %(message)s'
-    logging.basicConfig(level=private.LEVEL,
-                        format=format,
-                        datefmt='%a, %d %b %Y %H:%M:%S',
-                        filename=private.FILE,
-                        filemode='w')
     bw = BooneWeather(username=username, password=password)
 
