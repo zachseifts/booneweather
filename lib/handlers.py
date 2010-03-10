e7ce24a384627380ebc8fda034a04b8872aeb85d
Zach Seifts <zach.seifts@gmail.com>
2010-03-10 13:45:04 -0500

    cleaning up the dm handler, removing clean method and doing it after it sends a message in case something fails.

--- lib/handlers.py
+++ lib/handlers.py
@@ -7,21 +7,16 @@ class DirectMessageHandler(object):
         self.api = twitter.Api(username=username, password=password)
         self.get_messages()
         self.respond()
-        self.clean()
 
     def get_messages(self):
         ''' Gets a list of the direct messages. '''
         self.messages = self.api.GetDirectMessages()
 
     def respond(self):
-        ''' Responds to each direct message. '''
+        ''' Responds to each direct message and deletes it. '''
         for m in self.messages:
             message = 'wait, this works???? (not yet, but soon)'
             user = m.sender_screen_name
             self.api.PostDirectMessage(user, message)
-
-    def clean(self):
-        ''' Deletes the direct messages after they have been replied to.'''
-        [self.api.DestroyDirectMessage(m.id) for m in self.messages]
-
+            self.api.DestroyDirectMessage(m.id)
 
